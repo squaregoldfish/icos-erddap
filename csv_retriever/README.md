e1b3c3b5b6ddd3cbebfaddee9095e4a246220e36
Steve Jones <steve@squaregoldfish.co.uk>
2024-05-15 14:47:59 +0200

    CSV Retriever flask app

---
+++ csv_retriever/README.md
@@ -0,0 +1,12 @@
+Flask script to retrieve CSV data from the Carbon Portal.
+
+URLS for this app must be of the form http://localhost:5000/<pid>
+The PID is same as that used in the URI for a Carbon Portal Data Object.
+
+If the object exists, the CSV content will be returned. Otherwise a response
+containing the status code and message from the underlying failure will be returned.
+
+
+Required pip packages:
+Flask
+icoscp_core
---
+++ csv_retriever/app.py
@@ -0,0 +1,35 @@
+#!bin/python
+"""
+This is a simple Flask app that retrieves the CSV content for a data object from the Carbon Portal.
+URLS for this app must be of the form http://localhost:5000/<pid>
+The PID is same as that used in the URI for a Carbon Portal Data Object.
+
+If the object exists, the CSV content will be returned. Otherwise a response
+containing the status code and message from the underlying failure will be returned.
+"""
+
+import os
+import re
+from flask import Flask
+from icoscp_core.icos import data
+from http.client import HTTPResponse
+
+app = Flask(__name__)
+
+@app.route("/<pid>")
+def get_csv(pid):
+    """
+    Retrieve the CSV data for the specified PID.
+    The PID is as used in the URI for a data object.
+    """
+    result = None
+
+    try:
+        result = data.get_csv_byte_stream(pid)
+    except Exception as e:
+        # Parse the exception string
+        regex = re.compile('.*HTTP response code: ([0-9]+), response: (.*)')
+        match = regex.match(e.args[0])
+        result = match[2], int(match[1]), []
+
+    return result
