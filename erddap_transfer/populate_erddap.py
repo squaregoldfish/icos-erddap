d7c51f381bd24f23eae0e06d8a0356689a4c6f89
Steve Jones <steve@squaregoldfish.co.uk>
2024-07-18 14:53:11 +0200

    Define datasets as Trajectory or TimeSeries

--- erddap_transfer/populate_erddap.py
+++ erddap_transfer/populate_erddap.py
@@ -8,6 +8,7 @@ import database
 import os
 import re
 import icos
+import html
 from icoscp_core.icos import data
 from Attribute import Attribute
 
@@ -96,18 +97,13 @@ def _make_soop_entry(pid, metadata, config):
 
     # Attributes
     entry += '<addAttributes>\n'
-    entry += _make_common_attributes(pid, metadata)
-
+    entry += _make_common_attributes(pid, metadata, 'Trajectory')
+    entry += _make_attribute_xml(Attribute('cdm_trajectory_variables', 'expocode'))
     entry += '</addAttributes>\n'
 
     # Data Variables
     entry += _generate_data_variables_xml('soop_variables.csv')
-
-    entry += _variable_from_global_attribute('expocode', 'EXPOCODE', 'EXPOCODE')
-    entry += _variable_from_global_attribute('platform_id', 'platform_id', 'ICES Platform Code')
-
-    # Manually created fixed values
-    # _trajectory_id (will be EXPO CODE)
+    entry += _make_expocode_variable('trajectory_id')
 
     return entry
 
@@ -118,15 +114,13 @@ def _make_fos_entry(pid, metadata, config):
 
     # Attributes
     entry += '<addAttributes>\n'
-    entry += _make_common_attributes(pid, metadata)
-
+    entry += _make_common_attributes(pid, metadata, 'TimeSeries')
+    entry += _make_attribute_xml(Attribute('cdm_timeseries_variables', 'expocode'))
     entry += '</addAttributes>\n'
 
     # Data Variables
     entry += _generate_data_variables_xml('fos_variables.csv')
-
-    # Manually created fixed values
-    # _trajectory_id (will be EXPO CODE)
+    entry += _make_expocode_variable('timeseries_id')
 
     return entry
 
@@ -134,7 +128,7 @@ def _make_fos_entry(pid, metadata, config):
 def _make_common_base(pid, config):
     base = '<reloadEveryNMinutes>10080</reloadEveryNMinutes>\n'
     base += '<updateEveryNMillis>10000</updateEveryNMillis>\n'
-    base += f'<fileDir>{os.path.join(config['datasets_dir'], pid)}</fileDir>\n'
+    base += f'<fileDir>{html.escape(os.path.join(config['datasets_dir'], pid))}</fileDir>\n'
     base += '<fileNameRegex>.*\\.csv</fileNameRegex>\n'
     base += '<recursive>false</recursive>\n'
     base += '<pathRegex>.*</pathRegex>\n'
@@ -150,8 +144,8 @@ def _make_common_base(pid, config):
     return base
 
 
-def _make_common_attributes(pid, metadata):
-    common_attributes = _make_attribute_xml(Attribute('cdm_data_type', 'Point'))
+def _make_common_attributes(pid, metadata, cdm_data_type):
+    common_attributes = _make_attribute_xml(Attribute('cdm_data_type', cdm_data_type))
     common_attributes += _make_attribute_xml(Attribute('Conventions', 'COARDS, CF-1.6, ACDD-1.3'))
     common_attributes += _make_attribute_xml(Attribute('infoUrl', icos.make_data_object_uri(pid)))
     common_attributes += _make_attribute_xml(Attribute('institution', 'ICOS RI; TK'))
@@ -161,10 +155,10 @@ def _make_common_attributes(pid, metadata):
     common_attributes += _make_attribute_xml(Attribute('sourceUrl', icos.make_data_object_uri(pid)))
     common_attributes += _make_attribute_xml(Attribute('standard_name_vocabulary', 'CF Standard Name Table v70'))
     common_attributes += _make_attribute_xml(Attribute('summary', 'TK'))
-    common_attributes += _make_attribute_xml(Attribute('title', f'TK - {metadata['data_object']['fileName']}'))
     common_attributes += _make_attribute_xml(
-        Attribute('expocode', f'{metadata['data_object']['fileName'].split(".")[0]}'))
-    common_attributes += _make_attribute_xml(Attribute('platform_id', f'{metadata['data_object']['fileName'][:4]}'))
+        Attribute('title', f'TK - {html.escape(metadata['data_object']['fileName'])}'))
+    common_attributes += _make_attribute_xml(
+        Attribute('citation', f'{html.escape(metadata['data_object']['citation'])}'))
 
     return common_attributes
 
@@ -207,27 +201,11 @@ def _generate_data_variables_xml(var_file):
     return xml
 
 
-def _variable_from_global_attribute(attribute, destination, long_name):
-    xml = '<dataVariable>\n'
-    xml += f'<sourceName>global:{attribute}</sourceName>\n'
-    xml += f'<destinationName>{destination}</destinationName>\n'
-    xml += '<dataType>String</dataType>\n'
-
-    xml += '<addAttributes>\n'
-    xml += _make_attribute_xml(Attribute('ioos_category', 'Unknown'))
-    xml += _make_attribute_xml(Attribute('long_name', long_name))
-    xml += '</addAttributes>\n'
-
-    xml += '</dataVariable>\n'
-
-    return xml
-
-
 def _make_data_variable_xml(source_name, destination_name, data_type, units, ioos_category, long_name,
                             standard_name, other_attributes):
     xml = '<dataVariable>\n'
-    xml += f'<sourceName>{source_name}</sourceName>\n'
-    xml += f'<destinationName>{destination_name}</destinationName>\n'
+    xml += f'<sourceName>{html.escape(source_name)}</sourceName>\n'
+    xml += f'<destinationName>{html.escape(destination_name)}</destinationName>\n'
     xml += f'<dataType>{data_type}</dataType>\n'
 
     xml += '<addAttributes>\n'
@@ -257,10 +235,30 @@ def _make_attribute_xml(attribute):
     return attr
 
 
+def _make_expocode_variable(cf_role=None):
+    xml = '<dataVariable>\n'
+    xml += f'<sourceName>***fileName,(.*)\\.csv,1</sourceName>\n'
+    xml += f'<destinationName>expocode</destinationName>\n'
+    xml += '<dataType>String</dataType>\n'
+
+    xml += '<addAttributes>\n'
+    xml += _make_attribute_xml(Attribute('ioos_category', 'Unknown'))
+    xml += _make_attribute_xml(Attribute('long_name', 'EXPOCODE'))
+
+    if cf_role is not None:
+        xml += _make_attribute_xml(Attribute('cf_role', cf_role))
+
+    xml += '</addAttributes>\n'
+
+    xml += '</dataVariable>\n'
+
+    return xml
+
+
 def _write_datasets_xml(config, db):
     with open(config['datasets_xml_location'], 'w') as out:
         # Write the erddap preamble and open root node
-        out.write('<?xml version="1.0" encoding="ISO-8859-1" ?>\n')
+        out.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
         out.write('<erddapDatasets>\n')
 
         # Copy the contents of the start template into the file
